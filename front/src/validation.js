0a8c791a26f6edde0655cb7efd93f90ae28f28b7
GabiTom <gabrieltomasi22@gmail.com>
2023-08-03 16:00:28 -0300

    correccion validation

--- front/src/validation.js
+++ front/src/validation.js
@@ -1,18 +1,18 @@
 const validation = ({ email, password }) => {
-  const regExEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-] +@ [a-zA-Z0 -9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  const regExEmail = /\S+@\S+\.\S+/;
   const regexPassword = /^(?=.*\d).{5,9}$/
   let errors = {};
   if (!email){
     errors.email='El email no puede estar vacío'
   }
-  if (regExEmail.test(email)) {
+  if (!regExEmail.test(email)) {
     errors.email = "Ingrese un email válido";
   }
   if (email.length>35){
     errors.email = 'El email proporcionado es demaciado largo'
   }
   if (!regexPassword.test(password)){
-    errors.password = 'la contraseña debe contener al menos un número, y una longitud entre 6 y 10 caracteres'
+    errors.password = 'la contraseña debe contener al menos dos números, y una longitud entre 6 y 10 caracteres'
   }
   return errors
 };
