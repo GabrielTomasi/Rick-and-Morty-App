10eac918d63f8da5b534965d8ad552b003684ef1
GabiTom <gabrieltomasi22@gmail.com>
2023-11-06 11:08:48 -0300

    add: Globalprovider component

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -5,11 +5,12 @@ import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
-
+import { GlobalStyle } from './styled-components/LoginForm.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store = {store}>
   <BrowserRouter>
+  <GlobalStyle/>
     <App />
   </BrowserRouter>
   </Provider>
